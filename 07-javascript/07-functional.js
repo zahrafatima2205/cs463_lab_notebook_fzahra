1789a6f53db4ab4444cee183cf4a8124fc466759
Fatima Zahra <fzahra@pdx.edu>
2026-03-01 15:58:12 -0800

    completed 07-functional, formatted with prettier

--- 07-javascript/07-functional.js
+++ 07-javascript/07-functional.js
@@ -42,11 +42,6 @@ const reduceHouses = (characters) => {
   //const sum = characters.reduce(reducer, 0);
   //const sum = characters.reduce((acc, curr) => acc + curr.id, 10000);
   const houses = characters.reduce((acc, curr) => {
-    //if (acc.hasOwnProperty(curr.house)) {
-    //  acc[curr.house]++;
-    //} else {
-    //  acc[curr.house] = 1;
-    //}
     acc.hasOwnProperty(curr.house) ? acc[curr.house]++ : (acc[curr.house] = 1);
     return acc;
   }, {});
